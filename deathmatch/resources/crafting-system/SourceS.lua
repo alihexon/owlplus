64fce38c601925ce404dbcd03c0be4aa63c12596
alihexon <@alihexon@gmail.com>
2025-03-17 18:07:42 +0330

    crafting-system: fixed cords of marker

--- deathmatch/resources/crafting-system/SourceS.lua
+++ deathmatch/resources/crafting-system/SourceS.lua
@@ -1,4 +1,4 @@
-local markerX, markerY, markerZ = 2779.9921875, -2417.47265625, 13.635567665
+local markerX, markerY, markerZ = 2779.9921875, -2417.47265625, 13.635567665 -1
 local craftingMarker = createMarker(markerX, markerY, markerZ, "cylinder", 1.5, 255, 0, 0, 150)
 
 function onPlayerEnterMarker(hitPlayer, matchingDimension)
