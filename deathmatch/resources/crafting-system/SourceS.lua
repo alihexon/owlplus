e188cc08b6c55352e622b378414b9ac4f0ee66c5
alihexon <@alihexon@gmail.com>
2025-03-17 18:04:16 +0330

    crafting-system: fix double marker issue

--- deathmatch/resources/crafting-system/SourceS.lua
+++ deathmatch/resources/crafting-system/SourceS.lua
@@ -1,10 +1,9 @@
--- Server-side code
 local markerX, markerY, markerZ = 2779.9921875, -2417.47265625, 13.635567665
 local craftingMarker = createMarker(markerX, markerY, markerZ, "cylinder", 1.5, 255, 0, 0, 150)
 
 function onPlayerEnterMarker(hitPlayer, matchingDimension)
     if hitPlayer and getElementType(hitPlayer) == "player" and matchingDimension then
-        outputChatBox("Hello, " .. getPlayerName(hitPlayer) .. "! Welcome to the crafting area.", hitPlayer, 0, 255, 0)
+        triggerClientEvent(hitPlayer, "onClientPlayerEnterCraftingMarker", hitPlayer)
     end
 end
 
