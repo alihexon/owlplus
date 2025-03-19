fea4d69b2d1178acb0cba4fe81cd1f6387b21063
alihexon <@alihexon@gmail.com>
2025-03-19 16:50:39 +0330

    crafting-system: merge craftable items into server. #13

--- deathmatch/resource-cache/http-client-files/crafting-system/SourceC.lua
+++ deathmatch/resource-cache/http-client-files/crafting-system/SourceC.lua
@@ -1,31 +1,16 @@
 local screenW, screenH = guiGetScreenSize()
-local craftingWindow = nil
-local craftingList = nil
-local craftButton = nil
-local closeButton = nil
-
-local craftableItems = {
-    { name = "pcp", displayName = "PCP" },
-    { name = "redbandana", displayName = "Red Bandana" },
-    { name = "cocaine", displayName = "Cocaine" },
-    { name = "cocainee", displayName = "Cocaine Brick" },
-    { name = "meth", displayName = "Meth" },
-    { name = "heroin", displayName = "Heroin" },
-    { name = "heroinn", displayName = "Heroin Brick" }
-}
+local craftingWindow, craftingList, craftButton, closeButton
+local craftableItems = {}
 
 function openCraftingMenu()
-    if isElement(craftingWindow) then return end -- Prevent opening multiple windows
+    if isElement(craftingWindow) then return end -- Prevent multiple windows
+
+    triggerServerEvent("requestCraftableItems", localPlayer) -- Request item list
 
     craftingWindow = guiCreateWindow((screenW - 400) / 2, (screenH - 300) / 2, 400, 300, "Crafting Menu", false)
     craftingList = guiCreateGridList(10, 30, 380, 200, false, craftingWindow)
     
     guiGridListAddColumn(craftingList, "Item Name", 0.85)
-    
-    for _, item in ipairs(craftableItems) do
-        local row = guiGridListAddRow(craftingList)
-        guiGridListSetItemText(craftingList, row, 1, item.displayName, false, false)
-    end
 
     craftButton = guiCreateButton(10, 240, 180, 40, "Craft Item", false, craftingWindow)
     closeButton = guiCreateButton(210, 240, 180, 40, "Close", false, craftingWindow)
@@ -33,8 +18,8 @@ function openCraftingMenu()
     addEventHandler("onClientGUIClick", craftButton, function()
         local selectedRow = guiGridListGetSelectedItem(craftingList)
         if selectedRow ~= -1 then
-            local itemName = guiGridListGetItemText(craftingList, selectedRow, 1)
-            triggerServerEvent("requestCrafting", localPlayer, itemName) -- Send item name only
+            local itemID = guiGridListGetItemData(craftingList, selectedRow, 1)
+            triggerServerEvent("requestCrafting", localPlayer, itemID)
         else
             outputChatBox("Select an item to craft.", 255, 0, 0)
         end
@@ -46,5 +31,16 @@ function openCraftingMenu()
         end
     end, false)
 end
-
 addCommandHandler("craft", openCraftingMenu)
+
+addEvent("receiveCraftableItems", true)
+addEventHandler("receiveCraftableItems", root, function(serverItems)
+    craftableItems = serverItems
+    guiGridListClear(craftingList)
+    
+    for _, item in ipairs(craftableItems) do
+        local row = guiGridListAddRow(craftingList)
+        guiGridListSetItemText(craftingList, row, 1, item.displayName, false, false)
+        guiGridListSetItemData(craftingList, row, 1, item.id)
+    end
+end)
--- deathmatch/resources/crafting-system/SourceS.lua
+++ deathmatch/resources/crafting-system/SourceS.lua
@@ -1,24 +1,29 @@
--- Define craftable items with required materials (name first, then displayName)
+-- Define craftable items (server-side only)
 local craftableItems = {
-    { name = "pcp", displayName = "PCPp", id = 43, materials = { { id = 33, quantity = 2 }, { id = 34, quantity = 1 } } },       
-    { name = "redbandana", displayName = "Red Bandana", id = 123, materials = { { id = 160, quantity = 1 } } }, 
-    { name = "cocaine", displayName = "Cocaine", id = 50, materials = { { id = 42, quantity = 3 } } },
-    { name = "cocainee", displayName = "Cocaine Brick", id = 51, materials = { { id = 43, quantity = 2 } } },
-    { name = "meth", displayName = "Meth", id = 52, materials = { { id = 44, quantity = 2 }, { id = 42, quantity = 1 } } },
-    { name = "heroin", displayName = "Heroin", id = 53, materials = { { id = 45, quantity = 3 } } },
-    { name = "heroinn", displayName = "Heroin Brick", id = 54, materials = { { id = 46, quantity = 2 }, { id = 44, quantity = 1 } } }
+    { id = 1, name = "pcp", displayName = "PCP", itemID = 43, materials = { { id = 33, quantity = 2 }, { id = 34, quantity = 1 } } },       
+    { id = 2, name = "redbandana", displayName = "Red Bandana", itemID = 123, materials = { { id = 160, quantity = 1 } } }, 
+    { id = 3, name = "cocaine", displayName = "Cocaine", itemID = 50, materials = { { id = 42, quantity = 3 } } },
+    { id = 4, name = "cocainee", displayName = "Cocaine Brick", itemID = 51, materials = { { id = 43, quantity = 2 } } },
+    { id = 5, name = "meth", displayName = "Meth", itemID = 52, materials = { { id = 44, quantity = 2 }, { id = 42, quantity = 1 } } },
+    { id = 6, name = "heroin", displayName = "Heroin", itemID = 53, materials = { { id = 45, quantity = 3 } } },
+    { id = 7, name = "heroinn", displayName = "Heroin Brick", itemID = 54, materials = { { id = 46, quantity = 2 }, { id = 44, quantity = 1 } } }
 }
-
--- Create crafting marker
-local markerX, markerY, markerZ = 2779.9921875, -2417.47265625, 13.635567665 - 1
-local craftingMarker = createMarker(markerX, markerY, markerZ, "cylinder", 1.5, 255, 0, 0, 150)
 
 local playersInMarker = {}
 local activeCraftingSessions = {}
 
-function craftItem(player, itemName)
+-- Send craftable items to client
+addEvent("requestCraftableItems", true)
+addEventHandler("requestCraftableItems", root, function()
+    if client then
+        triggerClientEvent(client, "receiveCraftableItems", client, craftableItems)
+    end
+end)
+
+-- Crafting function
+function craftItem(player, itemID)
     if not playersInMarker[player] then
-        outputChatBox("You must be inside the crafting marker to use this command.", player, 255, 0, 0)
+        outputChatBox("You must be inside the crafting marker to craft.", player, 255, 0, 0)
         return
     end
 
@@ -29,14 +34,13 @@ function craftItem(player, itemName)
 
     local foundItem = nil
     for _, item in ipairs(craftableItems) do
-        if item.name:lower() == itemName:lower() then  -- Case-insensitive check
+        if item.id == itemID then
             foundItem = item
             break
         end
     end
 
     if foundItem then
-        -- Check if the player has the required materials
         for _, material in ipairs(foundItem.materials) do
             if exports["item-system"]:countItems(player, material.id) < material.quantity then
                 outputChatBox("You do not have enough materials for " .. foundItem.displayName .. ".", player, 255, 0, 0)
@@ -44,37 +48,25 @@ function craftItem(player, itemName)
             end
         end
 
-        -- Start crafting process
         activeCraftingSessions[player] = true
-        outputChatBox("Processing... Crafting " .. foundItem.displayName .. " (5 seconds).", player, 0, 255, 0)
+        outputChatBox("Crafting " .. foundItem.displayName .. "... (5 seconds)", player, 0, 255, 0)
 
         setTimer(function()
             if isElement(player) and playersInMarker[player] then
-                -- Recheck materials before finalizing
-                for _, material in ipairs(foundItem.materials) do
-                    if exports["item-system"]:countItems(player, material.id) < material.quantity then
-                        outputChatBox("Crafting failed! You lost or used some materials.", player, 255, 0, 0)
-                        activeCraftingSessions[player] = nil
-                        return
-                    end
-                end
-
-                -- Remove materials
                 for _, material in ipairs(foundItem.materials) do
                     for i = 1, material.quantity do
                         exports["item-system"]:takeItem(player, material.id)
                     end
                 end
 
-                -- Give crafted item
-                local giveSuccess = exports["item-system"]:giveItem(player, foundItem.id, 1)
+                local giveSuccess = exports["item-system"]:giveItem(player, foundItem.itemID, 1)
                 if giveSuccess then
                     outputChatBox("You crafted a " .. foundItem.displayName .. "!", player, 0, 255, 0)
                 else
                     outputChatBox("Failed to give item. Inventory might be full.", player, 255, 0, 0)
                 end
             else
-                outputChatBox("Crafting canceled. You left the crafting area.", player, 255, 0, 0)
+                outputChatBox("Crafting canceled. You left the area.", player, 255, 0, 0)
             end
             activeCraftingSessions[player] = nil
         end, 5000, 1)
@@ -82,25 +74,28 @@ function craftItem(player, itemName)
         return
     end
 
-    outputChatBox("Invalid item name.", player, 255, 0, 0)
+    outputChatBox("Invalid item ID.", player, 255, 0, 0)
 end
 
--- Marker events
-function onPlayerEnterMarker(hitPlayer, matchingDimension)
-    if hitPlayer and getElementType(hitPlayer) == "player" and matchingDimension then
+-- Marker system
+local markerX, markerY, markerZ = 2779.9921875, -2417.47265625, 13.635567665 - 1
+local craftingMarker = createMarker(markerX, markerY, markerZ, "cylinder", 1.5, 255, 0, 0, 150)
+
+function onPlayerEnterMarker(hitPlayer)
+    if getElementType(hitPlayer) == "player" then
         playersInMarker[hitPlayer] = true
-        outputChatBox("Type /craft to open the crafting menu.", hitPlayer, 0, 255, 0)
+        outputChatBox("Type /craft to open the menu.", hitPlayer, 0, 255, 0)
     end
 end
 addEventHandler("onMarkerHit", craftingMarker, onPlayerEnterMarker)
 
-function onPlayerLeaveMarker(hitPlayer, matchingDimension)
-    if hitPlayer and getElementType(hitPlayer) == "player" and matchingDimension then
+function onPlayerLeaveMarker(hitPlayer)
+    if getElementType(hitPlayer) == "player" then
         playersInMarker[hitPlayer] = nil
         outputChatBox("You left the crafting area.", hitPlayer, 255, 0, 0)
 
         if activeCraftingSessions[hitPlayer] then
-            outputChatBox("Crafting canceled. You left the crafting area.", hitPlayer, 255, 0, 0)
+            outputChatBox("Crafting canceled. You left.", hitPlayer, 255, 0, 0)
             activeCraftingSessions[hitPlayer] = nil
         end
     end
@@ -109,8 +104,8 @@ addEventHandler("onMarkerLeave", craftingMarker, onPlayerLeaveMarker)
 
 -- Handle crafting request
 addEvent("requestCrafting", true)
-addEventHandler("requestCrafting", root, function(itemName)
+addEventHandler("requestCrafting", root, function(itemID)
     if client then
-        craftItem(client, itemName)
+        craftItem(client, itemID)
     end
 end)
